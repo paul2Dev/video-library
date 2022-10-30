5144d0447708574ae4242ec49695ea3b498c4db5
Paul2D <paul.maxineanu@gmail.com>
2022-10-30 15:43:46 +0200

    install and configure tailwind

---
+++ tailwind.config.cjs
@@ -0,0 +1,10 @@
+// tailwind.config.css
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./app.html", "./src/**/*.{svelte,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
