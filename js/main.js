6d0abdfd692a6719d1c39f20c8220c79f838aab0
Paul2D <paul.maxineanu@gmail.com>
2022-12-12 11:49:59 +0200

    refactor code into functions

--- js/main.js
+++ js/main.js
@@ -4,23 +4,17 @@ const input = document.querySelector('#newVideo');
 const button = document.querySelector('#addVideo');
 const select = document.querySelector('#videoCategory');
 
-const videos = JSON.parse(localStorage.getItem("videos") || "{}")
+
+const videos = JSON.parse(localStorage.getItem("videos") || JSON.stringify({}))
 
 const categories = {
   'gaming': [],
   'coding': [],
   'photography': [],
+  'gym': [],
 };
 
-for(const category in categories) {
-  const option = document.createElement('option');
-  option.value = category;
-  option.innerHTML = category;
-  select.appendChild(option);
-  if(!videos[category]){
-    videos[category] = [];
-  }
-};
+initLocalStorageAndCategoriesSelect(categories, videos, select);
 
 button.addEventListener('click', function(e) {
   e.preventDefault();
@@ -42,38 +36,65 @@ button.addEventListener('click', function(e) {
 
 });
 
+renderVideos();
+
+
+// Functions
+
+function initLocalStorageAndCategoriesSelect(categories, videos, select) {
+  
+  for(const category in categories) {
+    const option = document.createElement('option');
+    option.value = category;
+    option.innerHTML = category;
+    select.appendChild(option);
+    if(!videos[category]){
+      videos[category] = [];
+    }
+  };
+
+  localStorage.setItem("videos", JSON.stringify(videos));
+}
+
+
 function renderVideos() {
   const videosContainer = document.querySelector('#videos');
   videosContainer.innerHTML = '';
-
 
   for(const category in videos) {
 
     const categoryContainer = document.createElement('div');
     categoryContainer.classList.add('category');
 
-    const categoryTitle = document.createElement('h2');
-    categoryTitle.classList.add('category-title', 'text-2xl', 'font-bold', 'my-4');
-    categoryTitle.innerHTML = category;
-    categoryContainer.appendChild(categoryTitle);
+    renderCategoryTitle(categoryContainer, category);
 
-    
-    const videosGrid = document.createElement('div');
-    videosGrid.classList.add('videos', 'grid', 'grid-cols-4', 'gap-10');
-    categoryContainer.appendChild(videosGrid);
-
-    videos[category].forEach( video => {
-      const videoContainer = document.createElement('div');
-      videoContainer.innerHTML = `
-        <iframe width="360" height="280" src="https://www.youtube.com/embed/${video}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-      `;
-      videosGrid.appendChild(videoContainer);
-    });
+    renderVideosCategoryGrid(categoryContainer, category);
 
     videosContainer.appendChild(categoryContainer);
 
   }
 
+}
+
+function renderCategoryTitle(parentContainer, category) {
+  const categoryTitle = document.createElement('h2');
+  categoryTitle.classList.add('category-title', 'text-2xl', 'font-bold', 'my-4');
+  categoryTitle.innerHTML = category;
+  parentContainer.appendChild(categoryTitle);
+}
+
+function renderVideosCategoryGrid(parentContainer, category) {
+  const videosGrid = document.createElement('div');
+  videosGrid.classList.add('videos', 'grid', 'grid-cols-4', 'gap-10');
+  parentContainer.appendChild(videosGrid);
+
+  videos[category].forEach( video => {
+    const videoContainer = document.createElement('div');
+    videoContainer.innerHTML = `
+      <iframe width="360" height="280" src="https://www.youtube.com/embed/${video}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    `;
+    videosGrid.appendChild(videoContainer);
+  });
 }
 
 function getYoutubeVideoId(url){
@@ -82,4 +103,3 @@ function getYoutubeVideoId(url){
   return (match&&match[7].length==11)? match[7] : false;
 }
 
-renderVideos();
