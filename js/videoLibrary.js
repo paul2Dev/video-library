7b120207137941c95b531115df1870de85a3d88a
Paul2D <paul.maxineanu@gmail.com>
2022-12-27 13:55:02 +0200

    remove drag and drop from all listing

--- js/videoLibrary.js
+++ js/videoLibrary.js
@@ -284,7 +284,7 @@ export default class videoLibrary {
                 this.registerAddVideoToModalEvent(video, videoContainer);
             });
 
-            this.addDragAndDropEvents();
+            //this.addDragAndDropEvents();
         }
         
     }
