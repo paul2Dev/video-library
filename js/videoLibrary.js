df842dd47b26ded4f5d5a170130d80f0b61d955b
Paul2D <paul.maxineanu@gmail.com>
2023-01-04 15:30:46 +0200

    integrate delete category

--- js/videoLibrary.js
+++ js/videoLibrary.js
@@ -289,6 +289,7 @@ export default class videoLibrary {
 
     renderCategoryVideos(videosContainer, renderCategory) {
         this.renderCategoryTitle(videosContainer, renderCategory);
+        this.renderDeleteCategoryButton(videosContainer, renderCategory);
         this.renderVideosCategoryGrid(videosContainer, renderCategory);
     }
 
@@ -297,6 +298,17 @@ export default class videoLibrary {
         categoryTitle.classList.add('category-title', 'text-2xl', 'font-bold', 'my-4');
         categoryTitle.innerHTML = category;
         parentContainer.appendChild(categoryTitle);
+    }
+
+    renderDeleteCategoryButton(parentContainer, category) {
+        if(category != 'all') {
+            const deleteCategoryButton = document.createElement('button');
+            deleteCategoryButton.classList.add('delete-category', 'text-sm', 'text-red-500', 'hover:text-red-700', 'font-medium', 'ml-2');
+            deleteCategoryButton.innerHTML = `<svg aria-hidden="true" class="w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>`;
+            parentContainer.querySelector('.category-title').appendChild(deleteCategoryButton);
+
+            this.registerDeleteCategoryEvent(deleteCategoryButton, category);
+        }
     }
 
     renderVideosCategoryGrid(parentContainer, category) {
@@ -385,6 +397,18 @@ export default class videoLibrary {
             
             this.updateStorage(videos);
             this.renderVideos(category);
+        }.bind(this));
+    }
+
+    registerDeleteCategoryEvent(deleteCategoryButton, category) {
+        deleteCategoryButton.addEventListener('click', function(e) {
+            e.preventDefault();
+            if(confirm('Are you sure you want to delete this category?')) {
+                delete this.videos[category];
+                delete this.categories[category];
+                this.updateStorage(this.videos);
+                this.init();
+            }
         }.bind(this));
     }
 
